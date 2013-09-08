125e1f9988c48db1fef003ca575a1682143b54e8
Tobias Gesellchen <tobias@gesellix.de>
2013-09-09 00:09:25 +0200

    create minimal .deb file (not lintian valid)

--- src/main/groovy/de/gesellix/gradle/debian/BuildDebianPackageTask.groovy
+++ src/main/groovy/de/gesellix/gradle/debian/BuildDebianPackageTask.groovy
@@ -1,11 +1,39 @@
 package de.gesellix.gradle.debian
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.vafer.jdeb.Console
+import org.vafer.jdeb.DataProducer
+import org.vafer.jdeb.Processor
+import org.vafer.jdeb.utils.MapVariableResolver
+
+import static org.vafer.jdeb.Compression.GZIP
 
 class BuildDebianPackageTask extends DefaultTask {
 
+  @InputFiles
+  File[] controlFiles
+  @Input
+  DataProducer[] dataProducers
+  @OutputFile
+  File outputFile
+
+  BuildDebianPackageTask() {
+  }
+
   @TaskAction
   def buildPackage() {
+    def processor = new Processor([
+                                      info: { msg -> logger.info(msg) },
+                                      warn: { msg -> logger.warn(msg) }] as Console,
+                                  new MapVariableResolver([
+                                      name: "test-name",
+                                      version: "42"]))
+
+    def packageDescriptor = processor.createDeb(controlFiles, dataProducers, outputFile, GZIP)
+//    processor.createChanges(packageDescriptor, null, null, null, null, )
   }
 }
--- src/test/groovy/de/gesellix/gradle/debian/BuildDebianPackageTaskTest.groovy
+++ src/test/groovy/de/gesellix/gradle/debian/BuildDebianPackageTaskTest.groovy
@@ -3,6 +3,8 @@ package de.gesellix.gradle.debian
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.testng.annotations.Test
+import org.vafer.jdeb.mapping.Mapper
+import org.vafer.jdeb.producers.DataProducerFile
 
 class BuildDebianPackageTaskTest {
 
@@ -11,5 +13,17 @@ class BuildDebianPackageTaskTest {
     Project project = ProjectBuilder.builder().build()
     def task = project.task('buildDeb', type: BuildDebianPackageTask)
     assert task instanceof BuildDebianPackageTask
+  }
+
+  @Test(dependsOnMethods = ["can add task to project"])
+  public void "can run"() {
+    Project project = ProjectBuilder.builder().build()
+    def task = project.task('buildDeb', type: BuildDebianPackageTask)
+    task.controlFiles = new File("./src/test/resources/debian/control").listFiles()
+    task.dataProducers = [
+        new DataProducerFile(new File("./src/test/resources/debian/input.txt"), "/tmp", [] as String[], [] as String[], [] as Mapper[])
+    ]
+    task.outputFile = new File("./build/output.deb")
+    task.buildPackage()
   }
 }
