a85a8a4a6421b70f1bd4c1ab5f5a8ef31124c82d
Tobias Gesellchen <tobias@gesellix.de>
2013-09-09 21:50:29 +0200

    test improvement, added travis-ci config

--- src/test/groovy/de/gesellix/gradle/debian/BuildDebianPackageTaskTest.groovy
+++ src/test/groovy/de/gesellix/gradle/debian/BuildDebianPackageTaskTest.groovy
@@ -16,14 +16,22 @@ class BuildDebianPackageTaskTest {
   }
 
   @Test(dependsOnMethods = ["can add task to project"])
-  public void "can run"() {
+  public void "can create output file"() {
+    def outputFile = new File("./build/output.deb")
+    if (outputFile.exists()) {
+      outputFile.delete()
+    }
+    assert !outputFile.exists()
+
     Project project = ProjectBuilder.builder().build()
     def task = project.task('buildDeb', type: BuildDebianPackageTask)
     task.controlFiles = new File("./src/test/resources/debian/control").listFiles()
     task.dataProducers = [
         new DataProducerFile(new File("./src/test/resources/debian/input.txt"), "/tmp", [] as String[], [] as String[], [] as Mapper[])
     ]
-    task.outputFile = new File("./build/output.deb")
+    task.outputFile = outputFile
     task.buildPackage()
+
+    assert outputFile.exists()
   }
 }
