9b569ff67b4e170c8815cfde046a55eabce03a20
Tobias Gesellchen <tobias@gesellix.de>
2013-11-26 20:45:18 +0100

    declare shared test resources as @Shared

--- src/test/groovy/de/gesellix/gradle/debian/DebianPackagePluginSpec.groovy
+++ src/test/groovy/de/gesellix/gradle/debian/DebianPackagePluginSpec.groovy
@@ -5,6 +5,7 @@ import de.gesellix.gradle.debian.tasks.data.Data
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Shared
 import spock.lang.Specification
 
 import static de.gesellix.gradle.debian.DebianPackagePluginExtension.DEBPKGPLUGIN_EXTENSION_NAME
@@ -12,8 +13,10 @@ import static de.gesellix.gradle.debian.tasks.BuildDebianPackageTask.DEBPKGTASK_
 
 class DebianPackagePluginSpec extends Specification {
 
+  @Shared
   Project project
-  File projectDir
+  @Shared
+  def projectDir
 
   def setup() {
     URL resource = getClass().getResource('/gradle/build.gradle')
