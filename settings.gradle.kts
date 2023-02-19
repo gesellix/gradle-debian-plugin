88cf9960d627c839f4650444e95d2817d8fe8062
Tobias Gesellchen <tobias@gesellix.de>
2023-02-19 11:53:33 +0100

    configure toolchains resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "gradle-debian-plugin"
+
+// https://docs.gradle.org/8.0.1/userguide/toolchains.html#sub:download_repositories
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
