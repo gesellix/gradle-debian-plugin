61a325710cae002c68854b4e6ef2a0d1ff0a9750
Tobias Gesellchen <tobias@gesellix.de>
2024-09-15 12:00:52 +0200

    Bump Spock framework to 2.3-groovy-3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ dependencies {
   implementation("org.apache.commons:commons-compress:1.27.0")
   implementation("commons-io:commons-io:2.16.1")
 
-  testImplementation("org.spockframework:spock-core:2.1-groovy-3.0")
+  testImplementation("org.spockframework:spock-core:2.3-groovy-3.0")
   testImplementation("cglib:cglib-nodep:3.3.0")
 
   // see https://docs.gradle.org/current/userguide/test_kit.html
